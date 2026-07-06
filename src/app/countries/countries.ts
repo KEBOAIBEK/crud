6864f8bb8830f749192b014a19e7f5955c795168
KEBOAIBEK <oybek.ayubov.1306@gmail.com>
2026-07-06 18:11:08 +0500

    feat: implement countries component with grid view and CRUD creation modal

--- src/app/countries/countries.ts
+++ src/app/countries/countries.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { LowerCasePipe } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import {
   ChangeDetectionStrategy,
   Component,
@@ -7,19 +8,26 @@ import {
   OnInit,
   signal,
 } from '@angular/core';
-import { subscribe } from 'diagnostics_channel';
 
 @Component({
   selector: 'app-countries',
-  imports: [LowerCasePipe],
+  imports: [LowerCasePipe, ReactiveFormsModule],
   templateUrl: './countries.html',
   styleUrl: './countries.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Countries implements OnInit {
   private http = inject(HttpClient);
+  private fb = inject(FormBuilder);
 
   countries = signal<any[]>([]);
+  isModalOpen = signal(false);
+
+  form = this.fb.group({
+    code: this.fb.control('', [Validators.required]),
+    country: this.fb.control('', [Validators.required]),
+    description: this.fb.control(''),
+  });
 
   ngOnInit(): void {
     this.http
@@ -29,10 +37,29 @@ export class Countries implements OnInit {
       });
   }
 
-  create() {
-    const model = {};
+  submit() {
+    const model = this.form.getRawValue();
     this.http
       .post('https://6a3a0ce7917c7b14c74c9ee6.mockapi.io/Country', model)
-      .subscribe();
+      .subscribe({
+        next: (response) => {
+          if (response) {
+            this.countries.update((list) => [response, ...list]);
+            this.closeModal();
+            this.form.reset();
+          }
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
+
+  openModal() {
+    this.isModalOpen.set(true);
+  }
+
+  closeModal() {
+    this.isModalOpen.set(false);
   }
 }
