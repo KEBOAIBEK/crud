5eba08dd3df36a70d2ed561b85712332a8640516
KEBOAIBEK <oybek.ayubov.1306@gmail.com>
2026-07-06 17:02:44 +0500

    feat: implement countries module with CRUD functionality and HTTP client configuration

--- src/app/countries/countries.ts
+++ src/app/countries/countries.ts
@@ -1,10 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LowerCasePipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { subscribe } from 'diagnostics_channel';
 
 @Component({
   selector: 'app-countries',
-  imports: [],
-  template: `<p>countries works!</p>`,
+  imports: [LowerCasePipe],
+  templateUrl: './countries.html',
   styleUrl: './countries.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Countries {}
+export class Countries implements OnInit {
+  private http = inject(HttpClient);
+
+  countries = signal<any[]>([]);
+
+  ngOnInit(): void {
+    this.http
+      .get<any[]>('https://6a3a0ce7917c7b14c74c9ee6.mockapi.io/Country')
+      .subscribe((country) => {
+        this.countries.set(country);
+      });
+  }
+
+  create() {
+    const model = {};
+    this.http
+      .post('https://6a3a0ce7917c7b14c74c9ee6.mockapi.io/Country', model)
+      .subscribe();
+  }
+}
